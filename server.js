d23eb7c351f24c7585ffb3523d432d3e18da0107
Matcha Sai Sujan <116907747+matchasai@users.noreply.github.com>
2025-07-07 19:34:37 +0530

    Final push

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use("/uploads", express.static(uploadsPath));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Update with actual frontend URL
+    origin: ["*"], // Update with actual frontend URL
     credentials: true, 
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Content-Type, Authorization",
