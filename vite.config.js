95c9070be8006f39b7b66f68fea773be8eb9373b
Sai Sujan <matchasai03@gmail.com>
2025-08-23 07:20:12 +0530

    Updated admin branch

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });
